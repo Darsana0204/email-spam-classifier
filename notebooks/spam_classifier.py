7f3beb6c28bf8b08c0a0f5a737583651b88232cd
Darsana <darsanar1420@gmail.com>
2026-06-04 09:30:07 +0530

    Split features and labels

--- notebooks/spam_classifier.py
+++ notebooks/spam_classifier.py
@@ -26,5 +26,5 @@ print(y.head())
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2,random_state=42)
-print("\nYraining samples", len(X_train))
+print("\nTraining samples", len(X_train))
 print("Training samples:", len(X_test))
